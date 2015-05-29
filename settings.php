ca974aecffc0a00b4a92f18b8025b3a1d1196d21
pantobp <nemanjapantovic@live.ac.me>
2015-05-29 11:38:01 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,31 @@
+<?php
+	$adresa[0]="3960 Radio Road, Suite 202"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[0]="Naples, FL 34104";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[0]="Naples Corporate Office";
+	$telephone[0]=2396431990;// Broj koji se poziva na dugme CALL US
+
+	$adresa[1]="3425 Radio Road, Suite 201"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[1]="Naples, FL 34104";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[1]="Naples Design Center & Showroom";
+	$telephone[1]=2392617555;// Broj koji se poziva na dugme CALL US
+
+	$adresa[2]="13850 Treeline Road, Suite 1"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[2]="Ft Myers, FL 33913";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[2]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[2]="Ft Myers Design Center & Showroom";
+	$telephone[2]=2399358911;// Broj koji se poziva na dugme CALL US
+
+	$adresa[3]="919 North Collier Blvd"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[3]="Marco Island, FL 34145";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[3]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[3]="Marco Island Design Center & Showroom";
+	$telephone[3]=2399358911;// Broj koji se poziva na dugme CALL US
+
+
+	$centratelephone=2392449922;
+	$companynumber=4;
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
